0f2af5483c462a2bbd5e23f6dbe6f9382aa52230
Nan Xiao <road2stat@gmail.com>
2018-04-12 23:51:36 -0400

    add rstudio-daily-keep-config

---
+++ Casks/rstudio-daily-keep-config.rb
@@ -0,0 +1,10 @@
+cask 'rstudio-daily-keep-config' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.rstudio.org/download/latest/daily/desktop/mac/RStudio-latest.dmg'
+  name 'RStudio'
+  homepage 'https://www.rstudio.org/download/daily/desktop/mac/'
+
+  app 'RStudio.app'
+end
